a68e1a7d5ed8d187be27e3e635e29e0694a18c91
Timo Bingmann <tbgit@panthema.net>
2008-03-01 12:58:15 +0000

    Adding Serpent cipher implementation extracted from Botan and augmented with CBC mode.

---
+++ libenctain/serpent.h
@@ -0,0 +1,119 @@
+// $Id$
+
+#ifndef SERPENT_H
+#define SERPENT_H
+
+#include <stdint.h>
+#include <stdlib.h>
+
+namespace Enctain {
+
+/**
+ * Serpent encryption cipher state context to encrypt input data blocks in ECB
+ * (Electronic codebook) mode. Directly based on the code from the Botan
+ * cryptography library.
+ */
+class SerpentEncryptECB
+{
+private:
+    /// storage for the key schedule
+    uint32_t	round_key[132];
+
+public:
+
+    /// Set the encryption key. The key must be 16, 24 or 32 bytes long.
+    void set_key(const unsigned char* key, unsigned int keylen);
+
+    /// Encrypt a block of 16 bytes using the current cipher state.
+    void encrypt_block(const uint8_t src[16], uint8_t dst[16]) const;
+
+    /// Encrypt a length of n*16 bytes. Len must be a multiple of 16 or this
+    /// function will assert().
+    void encrypt(const void* src, void* dst, size_t len) const;
+};
+
+/**
+ * Serpent encryption cipher state context to decrypt input data blocks in ECB
+ * (Electronic codebook) mode. Directly based on the code from the Botan
+ * cryptography library.
+ */
+class SerpentDecryptECB
+{
+private:
+    /// storage for the key schedule
+    uint32_t	round_key[132];
+
+public:
+
+    /// Set the decryption key. The key must be 16, 24 or 32 bytes long.
+    void set_key(const unsigned char* key, unsigned int keylen);
+
+    /// Decrypt a block of 16 bytes using the current cipher state.
+    void decrypt_block(const uint8_t src[16], uint8_t dst[16]) const;
+
+    /// Decrypt a length of n*16 bytes. Len must be a multiple of 16 or this
+    /// function will assert().
+    void decrypt(const void* src, void* dst, size_t len) const;
+};
+
+/**
+ * Serpent encryption cipher state context to encrypt input data blocks in CBC
+ * (Cipher-block chaining) mode.
+ */
+class SerpentEncryptCBC
+{
+private:
+    /// storage for the key schedule
+    uint32_t	round_key[132];
+
+    /// cbc initialisation vector
+    uint32_t	l_cbciv[4];
+
+public:
+
+    /// Set the encryption key. The key must be 16, 24 or 32 bytes long.
+    void set_key(const unsigned char* key, unsigned int keylen);
+
+    /// Set the initial cbc vector. The vector is always 16 bytes long.
+    void set_cbciv(const uint8_t iv[16]);
+
+    /// Encrypt a block of 16 bytes using the current cipher state.
+    void encrypt_block(const uint8_t src[16], uint8_t dst[16]);
+
+    /// Encrypt a length of n*16 bytes. Len must be a multiple of 16 or this
+    /// function will assert().
+    void encrypt(const void* src, void* dst, size_t len);
+};
+
+/**
+ * Serpent encryption cipher state context to decrypt input data blocks in CBC
+ * (Cipher-block chaining) mode.
+ */
+class SerpentDecryptCBC
+{
+private:
+    /// storage for the key schedule
+    uint32_t	round_key[132];
+
+    /// cbc initialisation vector
+    uint32_t	l_cbciv[4];
+
+public:
+
+    /// Set the decryption key. The key must be 16, 24 or 32 bytes long.
+    void set_key(const unsigned char* key, unsigned int keylen);
+
+    /// Set the initial cbc vector. The vector is always 16 bytes long.
+    void set_cbciv(const uint8_t iv[16]);
+
+    /// Decrypt a block of 16 bytes using the current cipher state.
+    void decrypt_block(const uint8_t src[16], uint8_t dst[16]);
+
+    /// Decrypt a length of n*16 bytes. Len must be a multiple of 16 or this
+    /// function will assert().
+    void decrypt(const void* src, void* dst, size_t len);
+};
+
+} // namespace Enctain
+
+#endif // SERPENT_H
---
+++ libenctain/testsuite/test_serpent_cbc.cpp
@@ -0,0 +1,101 @@
+// $Id$
+
+#include "../serpent.h"
+
+#include <stdlib.h>
+#include <time.h>
+#include <assert.h>
+#include <iostream>
+
+#include <gcrypt.h>
+
+int main()
+{
+    char* key = "BVksLhOTmqxETMvfnbhE3xxx2RLRI52H";
+    char* iv = "ABCDEFGHIJKLMNOP";
+
+    std::cout << "Testing Serpent CBC implementation against libgcrypt...";
+    std::cout.flush();
+
+    // Create contexts
+
+    Enctain::SerpentEncryptCBC encctx1;
+
+    encctx1.set_key((uint8_t*)key, 256);
+    encctx1.set_cbciv((uint8_t*)iv);
+
+    Enctain::SerpentDecryptCBC decctx1;
+
+    decctx1.set_key((uint8_t*)key, 256);
+    decctx1.set_cbciv((uint8_t*)iv);
+
+    // Create libgcrypt Serpent context
+
+    gcry_cipher_hd_t encctx2;
+
+    gcry_error_t gcryerr = gcry_cipher_open(&encctx2, GCRY_CIPHER_SERPENT256, GCRY_CIPHER_MODE_CBC, 0);
+    if (gcryerr != 0) return -1;
+
+    gcryerr = gcry_cipher_setkey(encctx2, (uint8_t*)key, 32);
+    if (gcryerr != 0) return -1;
+
+    gcryerr = gcry_cipher_setiv(encctx2, (uint8_t*)iv, 16);
+    if (gcryerr != 0) return -1;
+
+    gcry_cipher_hd_t decctx2;
+
+    gcryerr = gcry_cipher_open(&decctx2, GCRY_CIPHER_SERPENT256, GCRY_CIPHER_MODE_CBC, 0);
+    if (gcryerr != 0) return -1;
+
+    gcryerr = gcry_cipher_setkey(decctx2, (uint8_t*)key, 32);
+    if (gcryerr != 0) return -1;
+
+    gcryerr = gcry_cipher_setiv(decctx2, (uint8_t*)iv, 16);
+    if (gcryerr != 0) return -1;
+
+    // Encrypt and Decrypt random data using both contexts.
+
+    srand( time(NULL) );
+
+    const unsigned int datalen = 16 * 1024;
+
+    uint8_t odata[datalen];
+    uint8_t ctdata1[datalen], ctdata2[datalen];
+    uint8_t ptdata1[datalen], ptdata2[datalen];
+
+    for (unsigned int ri = 0; ri < 1000; ++ri)
+    {
+	// create random data buffer
+	for (unsigned int i = 0; i < datalen; ++i) {
+	    odata[i] = (uint8_t)rand();
+	}
+
+	// encrypt data
+	encctx1.encrypt(odata, ctdata1, datalen);
+
+	gcry_cipher_encrypt(encctx2, ctdata2, datalen, odata, datalen);
+
+	// compare cipher texts
+	for (unsigned int i = 0; i < datalen; ++i) {
+	    assert(ctdata1[i] == ctdata2[i]);
+	}
+
+	// decrypt data
+	decctx1.decrypt(ctdata1, ptdata1, datalen);
+
+	gcry_cipher_decrypt(decctx2, ptdata2, datalen, ctdata2, datalen);
+	
+	// compare plain texts
+	for (unsigned int i = 0; i < datalen; ++i) {
+	    assert(odata[i] == ptdata1[i]);
+	    assert(ptdata1[i] == ptdata2[i]);
+	}
+    }
+
+    gcry_cipher_close(encctx2);
+    gcry_cipher_close(decctx2);
+
+    std::cout << "OK\n";
+
+    return 0;
+}
