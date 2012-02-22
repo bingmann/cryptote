1d0672ba7f8cb5987f688940e25d9729bc0b6ebe
Timo Bingmann <tbgit@panthema.net>
2012-02-22 21:25:22 +0000

    Fixing autoconf target cpu detection for arm compilation.

--- libenctain/botan-1.6/modules/tm_hard/tm_hard.cpp
+++ libenctain/botan-1.6/modules/tm_hard/tm_hard.cpp
@@ -12,28 +12,44 @@ namespace Botan {
 /*************************************************
 * Get the timestamp                              *
 *************************************************/
-u64bit Hardware_Timer::clock() const
-   {
-   u64bit rtc = 0;
-
 #if defined(BOTAN_TARGET_ARCH_IS_IA32) || defined(BOTAN_TARGET_ARCH_IS_AMD64)
-   u32bit rtc_low = 0, rtc_high = 0;
-   asm volatile("rdtsc" : "=d" (rtc_high), "=a" (rtc_low));
-   rtc = ((u64bit)rtc_high << 32) | rtc_low;
+
+u64bit Hardware_Timer::clock() const
+{
+    u32bit rtc_low = 0, rtc_high = 0;
+    asm volatile("rdtsc" : "=d" (rtc_high), "=a" (rtc_low));
+    return ((u64bit)rtc_high << 32) | rtc_low;
+}
+
 #elif defined(BOTAN_TARGET_ARCH_IS_PPC) || defined(BOTAN_TARGET_ARCH_IS_PPC64)
-   u32bit rtc_low = 0, rtc_high = 0;
-   asm volatile("mftbu %0; mftb %1" : "=r" (rtc_high), "=r" (rtc_low));
-   rtc = ((u64bit)rtc_high << 32) | rtc_low;
+
+u64bit Hardware_Timer::clock() const
+{
+    u32bit rtc_low = 0, rtc_high = 0;
+    asm volatile("mftbu %0; mftb %1" : "=r" (rtc_high), "=r" (rtc_low));
+    return ((u64bit)rtc_high << 32) | rtc_low;
+}
+
 #elif defined(BOTAN_TARGET_ARCH_IS_ALPHA)
-   asm volatile("rpcc %0" : "=r" (rtc));
+
+u64bit Hardware_Timer::clock() const
+{
+    u64bit rtc = 0;
+    asm volatile("rpcc %0" : "=r" (rtc));
+    return rtc;
+}
+
 #elif defined(BOTAN_TARGET_ARCH_IS_SPARC64)
-   asm volatile("rd %%tick, %0" : "=r" (rtc));
+
+u64bit Hardware_Timer::clock() const
+{
+    u64bit rtc = 0;
+    asm volatile("rd %%tick, %0" : "=r" (rtc));
+    return rtc;
+}
+
 #else
-   #error "Unsure how to access hardware timer on this system"
+   #warning "Unsure how to access hardware timer on this system"
 #endif
-
-   return rtc;
-   }
-
 }
 }
